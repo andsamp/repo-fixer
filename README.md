ad224b0c0a7554695e800f57cc7d644141d576d1
AndSamp <andsamp+envyzen@pm.me>
2020-08-24 22:04:13 -0500

    linting

---
+++ README.md
@@ -0,0 +1,22 @@
+## Configuration
+```json
+{
+  "baseDirectory": ".", //optional
+  "git": {
+    "mainBranch": "master", //optional
+    "remote": "origin", //optional
+    "newBranch": "[TIKT-405] updating dependencies",
+    "commitMessage": "adding dependencies for TIKT-405"
+  },
+  "projects": [
+    "project-1",
+    "project-two",
+    "third-project"
+  ],
+  "commands": [
+    "npm i @that/dep@1.0.1",
+    "npm i that-other-dep@4.7.1"
+  ],
+  "testCommand": "npm test" //optional
+}
+```
--- src/generate-tasks.js
+++ src/generate-tasks.js
@@ -4,68 +4,68 @@ const execa = require('execa')
 const Listr = require('listr')
 
 export const generateTasks = ({ mainBranch = 'master' }) => {
-    return new Listr([
-        {
-            title: 'Repository Prep',
-            task: () => {
-                return new Listr([
-                    {
-                        title: 'checking git status',
-                        task: () => execa.stdout('git', ['status', '--porcelain']).then(result => {
-                            if (result !== '') {
-                                ctx.stash = true
-                            }
-                        })
-                    },
-                    {
-                        title: 'stashing changes',
-                        enabled: ctx => ctx.stash === true,
-                        task: () => execa.stdout('git', ['stash'])
-                    },
-                    {
-                        title: 'checking out master',
-                        task: (ctx) => execa.stdout('git'['checkout', ctx.git.mainBranch])
-                    },
-                    {
-                        title: 'pulling remote changes',
-                        task: () => execa.stdout('git', ["pull"])
-                    },
-                    {
-                        title: 'creating local branch',
-                        task: () => execa.stdout('git', ["checkout", "-b", main.git.newBranch])
-                    }
-                ])
-            }
-        },
-        {
-            title: 'User commands',
-            task: (ctx) => {
-                return new Listr(generateUserCommands(ctx.userCommands))
-            }
-        },
-        {
-            title: 'Push Changes to remote',
-            task: () => {
-                return new Listr([
-                    {
-                        title: "Commit changes",
-                        task: (ctx) => execa.stdout('git', ["commit", "-am", ctx.git.commitMessage])
-                    },
-                    {
-                        title: "Push changes to remote",
-                        task: (ctx) => execa.stdout('git', ["push", "-u", ctx.git.remote, ctx.git.newBranch])
-                    }
-                ])
-            }
-        }
-    ])
+  return new Listr([
+    {
+      title: 'Repository Prep',
+      task: () => {
+        return new Listr([
+          {
+            title: 'checking git status',
+            task: (ctx) => execa.stdout('git', ['status', '--porcelain']).then(result => {
+              if (result !== '') {
+                ctx.stash = true
+              }
+            })
+          },
+          {
+            title: 'stashing changes',
+            enabled: ctx => ctx.stash === true,
+            task: () => execa.stdout('git', ['stash'])
+          },
+          {
+            title: 'checking out master',
+            task: (ctx) => execa.stdout('git', ['checkout', ctx.git.mainBranch])
+          },
+          {
+            title: 'pulling remote changes',
+            task: () => execa.stdout('git', ['pull'])
+          },
+          {
+            title: 'creating local branch',
+            task: (ctx) => execa.stdout('git', ['checkout', '-b', ctx.git.newBranch])
+          }
+        ])
+      }
+    },
+    {
+      title: 'User commands',
+      task: (ctx) => {
+        return new Listr(generateUserCommands(ctx.userCommands))
+      }
+    },
+    {
+      title: 'Push Changes to remote',
+      task: () => {
+        return new Listr([
+          {
+            title: 'Commit changes',
+            task: (ctx) => execa.stdout('git', ['commit', '-am', ctx.git.commitMessage])
+          },
+          {
+            title: 'Push changes to remote',
+            task: (ctx) => execa.stdout('git', ['push', '-u', ctx.git.remote, ctx.git.newBranch])
+          }
+        ])
+      }
+    }
+  ])
 }
 
 const generateUserCommands = userCommands => {
-    generateUserExecaCommands(userCommands).map(execaCommand => {
-        return {
-            title: execaCommand.title,
-            task: execa.stdout(...execaCommand.execaCommandParams)
-        }
-    })
+  generateUserExecaCommands(userCommands).map(execaCommand => {
+    return {
+      title: execaCommand.title,
+      task: execa.stdout(...execaCommand.execaCommandParams)
+    }
+  })
 }
