762c903eb3cdfddc69b875d1c8f4b4ea5aee493c
And Samp <andsamp+envyzen@pm.me>
2021-02-20 14:55:16 -0600

    remove reference to dist in src

--- src/task-lists/constants.js
+++ src/task-lists/constants.js
@@ -1,9 +1,9 @@
 import { GIT_MODE_CREATE, GIT_MODE_UPDATE } from '../utils/constants'
-import { checkGitStatus } from '../../dist/tasks/git/check-git-status'
 import { stashChanges } from '../tasks/git/stash-changes'
 import { checkoutDestinationBranch, checkoutMainBranch } from '../tasks/git/checkout-branch'
 import { pull } from '../tasks/git/pull'
 import { createLocalBranch } from '../tasks/git/create-local-branch'
+import { checkGitStatus } from '../tasks/git/check-git-status'
 
 export const REPO_PREP_TASKS = {
   [GIT_MODE_CREATE]: [
